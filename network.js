0e873dd50c53d777d486e29b1c0267a4f04d60ac
sneakzttv <sneakz@chainsafe.io>
2024-03-23 13:05:42 +0700

    Initialize

---
+++ network.js
@@ -0,0 +1,9 @@
+//You can see a list of compatible EVM chains at https://chainlist.org/
+window.networks = [
+  {
+    id: 80001,
+    label: "Mumbai Polygon",
+    token: "MATIC",
+    rpcUrl: 'https://rpc.ankr.com/polygon_mumbai',
+  }
+]
